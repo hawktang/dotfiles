7cc9790babc6a019475d6cfa15266c79fe4b2fb6
peter <peter_tang@singaporeair.com.sg>
2023-03-09 07:21:42 +0000

    init

---
+++ run_once_before_install-packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+brew bundle --no-lock --file=/dev/stdin <<EOF
+brew "git"
+brew "midnight-commander"
+brew "bat"
+brew "exa"
+brew "fd"
+brew "ripgrep"
+brew "fzf"
+brew "awscli"
+brew "chezmoi"
+brew "thefuck"
+brew "tldr"
+EOF
