62fc6445dc507825577b0c6ba72a1d5083319a40
hawktang <hawktang@gmail.com>
2024-02-09 02:03:19 +0800

    Update config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -5,5 +5,12 @@ alias ls='lsd'
 alias l='lsd -l'
 alias ll='lsd -lh'
 
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba init' !!
+set -gx MAMBA_EXE "/opt/homebrew/bin/micromamba"
+set -gx MAMBA_ROOT_PREFIX "/Users/hawktang/micromamba"
+$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+# <<< mamba initialize <<<
+
 atuin init fish | source
 starship init fish | source
