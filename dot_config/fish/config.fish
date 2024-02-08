8480d3912c5f2b4e38b8d922d66c96b5c3a7c9ac
hawktang <hawktang@gmail.com>
2024-02-09 01:50:25 +0800

    Update config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,5 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 alias ls='lsd'
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
 atuin init fish | source
 starship init fish | source
