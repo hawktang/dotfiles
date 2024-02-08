f6c122747e3f4735813187755b406a6483526074
hawktang <hawktang@gmail.com>
2024-02-09 01:56:43 +0800

    Update config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 alias ls='lsd'
-alias ll='lsd -l'
+alias l='lsd -l'
 alias ll='lsd -lh'
 
 atuin init fish | source
