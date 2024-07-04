880c2a721ac0b3d23d277e6c5210329208dc940d
hawktang <hawktang@gmail.com>
2024-07-04 15:39:49 +0800

    Update config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -29,6 +29,9 @@ alias conda='micromamba'
 # <<< mamba initialize <<<
 # --root-prefix $MAMBA_ROOT_PREFIX
 
+function __tabby_working_directory_reporting --on-event fish_prompt
+    echo -en "\e]1337;CurrentDir=$PWD\x7"
+end
 
 atuin init fish | source
 starship init fish | source
