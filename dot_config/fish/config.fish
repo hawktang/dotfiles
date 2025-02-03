39c7a6d298b35bbc0bc6aa871fca645dcf087a3d
hawktang <hawktang@gmail.com>
2025-02-03 14:15:23 +0800

    Update config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -48,9 +48,9 @@ atuin init fish | source
 starship init fish | source
 zoxide init fish | source
 
-set -gx http_proxy "http://127.0.0.1:6152"
-set -gx https_proxy "http://127.0.0.1:6152"
-set -gx no_proxy "localhost,127.0.0.1,.local,::1,!*.sq.com.sg"
+# set -gx http_proxy "http://127.0.0.1:6152"
+# set -gx https_proxy "http://127.0.0.1:6152"
+# set -gx no_proxy "localhost,127.0.0.1,.local,::1,!*.sq.com.sg"
 
 alias http="http --verify=no -pb"
 
