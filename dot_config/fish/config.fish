497a7f686812ea02aa746e49b9b0e3423d04aa06
hawktang <hawktang@gmail.com>
2025-08-04 18:19:00 +0800

    Update config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -47,7 +47,10 @@ zoxide init fish | source
 
 # set -gx http_proxy "http://127.0.0.1:6152"
 # set -gx https_proxy "http://127.0.0.1:6152"
-# set -gx no_proxy "localhost,127.0.0.1,.local,::1,!*.sq.com.sg"
+set -gx no_proxy "localhost,127.0.0.1,.local,::1,169.254.169.254"
+
+# 169.254.169.254 is where sia kriscloud get ec2 role.
+# !*.sq.com.sg
 
 
 alias http="http --verify=no -pb"
